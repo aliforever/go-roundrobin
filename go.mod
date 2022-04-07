a8c538327ef41dd3e95d0a427903b08b760099b8
aliforever <alipadida@live.com>
2022-04-07 18:12:43 +0430

    Hello Gorld!

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/aliforever/go-roundrobin
+
+go 1.18
---
+++ roundrobin.go
@@ -0,0 +1,58 @@
+package roundrobin
+
+import (
+	"errors"
+	"sync"
+)
+
+type RoundRobin struct {
+	m       sync.Mutex
+	counter int
+	items   []string
+}
+
+func New() *RoundRobin {
+	return &RoundRobin{}
+}
+
+func (r *RoundRobin) Add(items ...string) {
+	r.m.Lock()
+	defer r.m.Unlock()
+
+	items = append(items, items...)
+}
+
+func (r *RoundRobin) RemoveItem(item string) {
+	r.m.Lock()
+	defer r.m.Unlock()
+
+	newItems := []string{}
+
+	for _, i := range r.items {
+		if i != item {
+			newItems = append(newItems, i)
+		}
+	}
+
+	r.items = newItems
+}
+
+func (r *RoundRobin) Next() (item string, err error) {
+	r.m.Lock()
+	defer r.m.Unlock()
+
+	if len(r.items) == 0 {
+		err = errors.New("no_items_available")
+		return
+	}
+
+	if r.counter >= len(r.items) {
+		r.counter = 0
+	}
+
+	item = r.items[r.counter]
+
+	r.counter++
+
+	return
+}
